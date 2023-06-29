019c44bb6920c77bb47421943395304311de01aa
Louis Bailleau <louis.bailleau@free.fr>
2023-06-29 08:27:26 +0200

    feat: 🚧 login page form

--- frontend/src/app/modules/shared/pages/login/login.component.ts
+++ frontend/src/app/modules/shared/pages/login/login.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  protected loginForm: FormGroup;
+
+  public constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: [''],
+    });
+  }
+
+  protected onSubmit(): void {
+    //
+  }
+}
