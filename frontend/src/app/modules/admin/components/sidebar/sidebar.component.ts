1cbdfd7861702d70a7e5372687d4b616ec1e66d5
Louis Bailleau <louis.bailleau@free.fr>
2023-07-05 22:38:40 +0200

    feat: ✨ add links to admin sidebar

--- frontend/src/app/modules/admin/components/sidebar/sidebar.component.ts
+++ frontend/src/app/modules/admin/components/sidebar/sidebar.component.ts
@@ -1,29 +1,52 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  ViewChildren,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { APP_NAME } from '@app/app.module';
 import { User } from '@app/modules/shared/interfaces/user';
 import { AuthService } from '@app/modules/shared/services/auth/auth.service';
-import { ThemeService } from '@app/modules/shared/services/theme.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class AdminSidebarComponent {
-  protected user!: User;
+export class AdminSidebarComponent implements AfterViewInit, OnDestroy {
+  protected user?: User;
 
-  public constructor(
-    private authService: AuthService,
-    router: Router,
-    private themeService: ThemeService
-  ) {
-    if (!authService.userValue) {
-      void router.navigate(['/login']);
-      return;
-    }
+  @ViewChildren('details')
+  protected submenus: HTMLDetailsElement[] = [];
+
+  private authSub: Subscription;
+
+  public constructor(authService: AuthService, router: Router) {
+    this.authSub = authService.user.subscribe((user) => {
+      this.user = user;
+
+      if (!user) {
+        void router.navigate(['/login']);
+      }
+    });
 
     this.user = authService.userValue;
+  }
+
+  public ngAfterViewInit(): void {
+    for (const submenu of this.submenus) {
+      if (submenu.querySelector('[routerlinkactive]')) {
+        submenu.open = true;
+      } else {
+        submenu.open = false;
+      }
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.authSub.unsubscribe();
   }
 
   protected get appName(): string {
--- frontend/src/app/modules/admin/services/drawer.service.ts
+++ frontend/src/app/modules/admin/services/drawer.service.ts
@@ -1,17 +1,30 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class DrawerService {
+export class DrawerService implements OnDestroy {
   private _isOpen: BehaviorSubject<boolean>;
 
   public readonly events: Observable<boolean>;
 
-  public constructor() {
+  private routerSub: Subscription;
+
+  public constructor(private router: Router) {
     this._isOpen = new BehaviorSubject(false);
     this.events = this._isOpen.asObservable();
+
+    this.routerSub = this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.close();
+      }
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.routerSub.unsubscribe();
   }
 
   public get isOpen(): Observable<boolean> {
